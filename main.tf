d3c6c5caadd3f5a44a1588fc44e97a23413a3570
Ubuntu <azureuser@ci-master.seo2zebe0qnunoue5e3a3ekugd.hx.internal.cloudapp.net>
2026-05-28 01:46:44 +0000

    added terraform files

---
+++ main.tf
@@ -0,0 +1,37 @@
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "k8s-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "k8s-node"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_E2s_v3"
+
+  admin_username = "azureuser"
+  admin_password = "Password@1234"
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
